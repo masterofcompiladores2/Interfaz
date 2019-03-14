2f883cb38153dc61ec5f3cfefa9aefed45d06d05
emerson <emerson.riverar@pgn.gob.gt>
2019-03-14 08:26:43 -0600

    primer commit

---
+++ Creatorxml_200924451/src/creatorxml_200924451/Creatorxml_200924451.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package creatorxml_200924451;
+
+/**
+ *
+ * @author Emerson
+ */
+public class Creatorxml_200924451 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          pantallainicial y = new pantallainicial();
+          y.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
